4d1ccf80ad6c2f36c0921da9d25d865a6309fc60
tororutsu <20189326+tororutsu@users.noreply.github.com>
2026-06-06 18:29:39 -0400

    Added Vim Surround

--- plugin/30_plugins.lua
+++ plugin/30_plugins.lua
@@ -1,1 +1,7 @@
-vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+local gh = function(x) return 'https://github.com/' .. x end
+local cb = function(x) return 'https://codeberg.org/' .. x end
+
+vim.pack.add({
+    { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+    gh('tpope/vim-surround')
+})
